ab4373a41ef5ea2e4b7b4cc80a110cc4166ea0ad
squall7011 <gurdl7011@dsm.hs.kr>
2022-05-27 23:49:07 +0900

    Initial commit

--- src/main/java/com/ToDoApi/todo/databases/entity/ToDoList.java
+++ src/main/java/com/ToDoApi/todo/databases/entity/ToDoList.java
@@ -1,2 +1,13 @@
-package com.ToDoApi.todo.databases.entity;public class ToDoList {
+package com.ToDoApi.todo.databases.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "kanghyuk")
+@Entity
+public class ToDoList {
+    @Id
+    private Integer id;
+    private String contents;
 }
