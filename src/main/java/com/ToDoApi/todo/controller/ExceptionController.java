38f6504b15451979f150bba8df9a861aa39efd86
squall7011 <gurdl7011@dsm.hs.kr>
2022-05-30 23:36:04 +0900

    |POST| 에외처리 ControllerAdvice로 추가

---
+++ src/main/java/com/ToDoApi/todo/controller/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.ToDoApi.todo.controller;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity exception(NoSuchElementException error){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+    @ExceptionHandler(value = EmptyResultDataAccessException.class)
+    public ResponseEntity exception(EmptyResultDataAccessException error){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+}
