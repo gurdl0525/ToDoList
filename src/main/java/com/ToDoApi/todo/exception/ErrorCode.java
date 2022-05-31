3605835be9b4db21f13e96548904a5aa1d7e94db
squall7011 <gurdl7011@dsm.hs.kr>
2022-05-31 21:42:23 +0900

    |UPDATE| final 추가

--- src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
+++ src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     NOT_FOUND(404, "Not Found Id : 없는 아이디 입니다."),
     BAD_REQUEST(400, "Bad Request : 잘못된 입력입니다."),
     METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(400, "잘못된 엔드 포인트 입니다.");
-    private Integer status;
-    private String message;
+    private final Integer status;
+    private final String message;
 
     ErrorCode(int status, String message) {
         this.status = status;
