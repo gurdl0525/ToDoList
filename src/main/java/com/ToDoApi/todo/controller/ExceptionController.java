00c65d84947c71ed5b395c5af41ac621433e50e0
squall7011 <gurdl7011@dsm.hs.kr>
2022-05-31 21:43:05 +0900

    |UPDATE| MethodArgumentTypeMismatchException 익셉션 헨들러 추가

--- src/main/java/com/ToDoApi/todo/controller/ExceptionController.java
+++ src/main/java/com/ToDoApi/todo/controller/ExceptionController.java
@@ -1,9 +1,11 @@
 package com.ToDoApi.todo.controller;
 
 import com.ToDoApi.todo.exception.BaseException;
+import com.ToDoApi.todo.exception.ErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -12,5 +14,11 @@ public class ExceptionController {
     public ResponseEntity exception(BaseException error){
         return ResponseEntity.status(error.getErrorCode().getStatus())
                 .body(error.getErrorCode().getMessage());
+    }
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity exception(){
+        ErrorCode errorCode = ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION;
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(errorCode.getMessage());
     }
 }
