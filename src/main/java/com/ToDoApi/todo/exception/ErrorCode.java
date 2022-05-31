8663db2ad9b2c7dee450035da32c25b5ffb43028
squall7011 <gurdl7011@dsm.hs.kr>
2022-05-31 21:41:49 +0900

    |UPDATE| METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION 추가

--- src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
+++ src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
@@ -8,8 +8,8 @@ import java.util.NoSuchElementException;
 @Getter
 public enum ErrorCode {
     NOT_FOUND(404, "Not Found Id : 없는 아이디 입니다."),
-    BAD_REQUEST(400, "Bad Request : 잘못된 입력입니다.");
-
+    BAD_REQUEST(400, "Bad Request : 잘못된 입력입니다."),
+    METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(400, "잘못된 엔드 포인트 입니다.");
     private Integer status;
     private String message;
 
