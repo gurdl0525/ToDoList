d39c69845d25617272fb18cb028d3d2a7b977174
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-02 23:36:00 +0900

    |POST| Member에 대한 dto 생성

---
+++ src/main/java/com/ToDoApi/todo/dto/MemberRequest.java
@@ -0,0 +1,15 @@
+package com.ToDoApi.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberRequest {
+    private String memberId;
+    private String memberPsw;
+}
