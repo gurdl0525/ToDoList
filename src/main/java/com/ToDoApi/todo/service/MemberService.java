9b07718194cfa7b3a7437c9dd9646d0638102e4f
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-04 15:51:52 +0900

    |UPDATE| 회원 삭제코드, 패스워드 인코더 추가

--- src/main/java/com/ToDoApi/todo/service/MemberService.java
+++ src/main/java/com/ToDoApi/todo/service/MemberService.java
@@ -2,9 +2,10 @@ package com.ToDoApi.todo.service;
 
 import com.ToDoApi.todo.dto.MemberRequest;
 import com.ToDoApi.todo.dto.MessageResponse;
-import com.ToDoApi.todo.entity.Member;
+import com.ToDoApi.todo.entity.Member;;
 import com.ToDoApi.todo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
 
     @Transactional
@@ -21,13 +23,14 @@ public class MemberService {
         duplicateMemberCheck(request.getMemberId());
         Member member = Member.builder()
                 .memberId(request.getMemberId())
-                .password(request.getMemberPsw())
+                .password(passwordEncoder.encode(request.getMemberPsw()))
                 .build();
         memberRepository.save(member);
         return MessageResponse.builder()
                 .message(member.getMemberId() + "님이 정상적으로 회원가입 완료되었습니다.")
                 .build();
     }
+    @Transactional
     public List<String> findAll() {
         return memberRepository.findAll().stream()
                 .map(member -> member.getMemberId())
@@ -38,5 +41,19 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+    @Transactional
+    public MessageResponse deleteAll(){
+        memberRepository.deleteAll();
+        return MessageResponse.builder()
+                .message("모든 회원이 삭제 되었습니다.")
+                .build();
+    }
+    @Transactional
+    public MessageResponse deleteById(Long id){
+        memberRepository.deleteById(id);
+        return MessageResponse.builder()
+                .message("회원님이 정상적으로 탈퇴되었습니다.")
+                .build();
     }
 }
