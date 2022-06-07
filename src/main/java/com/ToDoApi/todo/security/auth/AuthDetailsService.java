2fa0bce74d77e12fe6b7131339dcd552b86e7c9e
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-07 23:35:57 +0900

    |POST| UserDetailsService 구현체 생성

---
+++ src/main/java/com/ToDoApi/todo/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.ToDoApi.todo.security.auth;
+
+import com.ToDoApi.todo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByMemberId(username)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("NOT FOUND USERNAME"));
+    }
+}
