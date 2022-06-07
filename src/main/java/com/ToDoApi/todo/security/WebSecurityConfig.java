bee86834fdf8105ed4cfd16a7164440cebd917a1
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-07 16:29:08 +0900

    |UPDATE| 파일 경로 수정

--- src/main/java/com/ToDoApi/todo/security/WebSecurityConfig.java
+++ src/main/java/com/ToDoApi/todo/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ToDoApi.todo.config;
+package com.ToDoApi.todo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
