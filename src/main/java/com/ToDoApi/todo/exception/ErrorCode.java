b1adf81b1aa539bb4d5f05ecb720405c92882f05
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-10 20:17:35 +0900

    |ADD| PASSWORD_NOT_MATCHED 에러코드 추가

--- src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
+++ src/main/java/com/ToDoApi/todo/exception/ErrorCode.java
@@ -4,14 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    NOT_FOUND(404, "NOT_FOUND : 없는 아이디 입니다."),
     BAD_REQUEST(400, "BAD_REQUEST : 잘못된 입력입니다."),
     METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(400, "잘못된 엔드포인트입니다."),
-    HTTP_MEDIA_TYPE_NOT_SUPPORTED_EXCEPTION(415, "BAD_REQUEST : 잘못된 입력 타입입니다."),
     ILLEGAL_STATE_EXCEPTION(400,"BAD_REQUEST : 중복된 아이디 입니다."),
     SQL_INTEGRITY_CONSTRAINT_VIOLATION_EXCEPTION(400, "BAD_REQUEST : 잘못된 객체 이름입니다."),
-    EMPTY_RESULT_DATA_ACCESS_EXCEPTION(400, "NOT_FOUND : 없는 아이디 입니다."),
+    PASSWORD_NOT_MATCHED(400, "BAD_REQUEST : 잘못된 비밀번호 입니다."),
     UN_AUTHORIZED_TOKEN_EXCEPTION(401, "인증되지 않은 토큰입니다."),
+    NOT_FOUND(404, "NOT_FOUND : 없는 아이디 입니다."),
+    HTTP_MEDIA_TYPE_NOT_SUPPORTED_EXCEPTION(415, "BAD_REQUEST : 잘못된 입력 타입입니다."),
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR");
     private final Integer status;
     private final String message;
