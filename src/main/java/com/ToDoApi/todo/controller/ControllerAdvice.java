d0cd662656b966251a7b52e6cd676dce2b0efab5
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-12 12:36:43 +0900

    |UPDATE| 리펙터링

--- src/main/java/com/ToDoApi/todo/controller/ControllerAdvice.java
+++ src/main/java/com/ToDoApi/todo/controller/ControllerAdvice.java
@@ -6,14 +6,13 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
-@ControllerAdvice
-public class ExceptionController {
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
 
     @ExceptionHandler(value = BaseException.class)
     public ResponseEntity exception(BaseException error){
