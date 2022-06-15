0e6575182926abdc8dfa6c165c7568fff71250c3
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-15 23:37:49 +0900

    |ADD| 토큰에서 Member를 찾아서 반환해주는 코드 생성

--- src/main/java/com/ToDoApi/todo/security/auth/AuthenticationFacade.java
+++ src/main/java/com/ToDoApi/todo/security/auth/AuthenticationFacade.java
@@ -1,16 +1,26 @@
 package com.ToDoApi.todo.security.auth;
 
+import com.ToDoApi.todo.entity.Member;
+import com.ToDoApi.todo.exception.BaseException;
+import com.ToDoApi.todo.exception.ErrorCode;
+import com.ToDoApi.todo.repository.MemberRepository;
+import com.ToDoApi.todo.security.JwtTokenProvider;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFacade {
-    @Value("${key}")
-    private String JWT_SECRET;
-    public String getAccountId(String token){
-        return Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token).getBody().getSubject();
+    private final MemberRepository memberRepository;
+    public Member getCurrentMember(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
+        return memberRepository.findByAccountId(authDetails.getUsername())
+                .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
     }
 }
