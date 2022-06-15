c31cbcc431c3874342e18e0b482ad7d9607f8890
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-15 23:41:00 +0900

    |ADD| 투두 생성시 is_success값 자동생성 추가,권한 검증 로직 추가

--- src/main/java/com/ToDoApi/todo/service/ToDoService.java
+++ src/main/java/com/ToDoApi/todo/service/ToDoService.java
@@ -1,26 +1,31 @@
 package com.ToDoApi.todo.service;
 
 import com.ToDoApi.todo.dto.*;
+import com.ToDoApi.todo.entity.Member;
 import com.ToDoApi.todo.entity.ToDoList;
 import com.ToDoApi.todo.repository.ToDoListRepository;
 import com.ToDoApi.todo.exception.BaseException;
 import com.ToDoApi.todo.exception.ErrorCode;
+import com.ToDoApi.todo.security.auth.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ToDoService {
     private final ToDoListRepository toDoListRepository;
-
+    private final AuthenticationFacade authenticationFacade;
     @Transactional
     public MessageResponse createToDo(CreateToDoRequest request){
         toDoListRepository.save(ToDoList.builder()
                     .contents(request.getContents())
-                    .is_success(false)
+                    .is_success(true)
+                    .member(authenticationFacade.getCurrentMember())
                     .build());
         return MessageResponse.builder()
                 .message("["+"ToDoList:" + request.getContents() +"]" + "가 생성이 완료되었습니다.")
@@ -30,24 +35,19 @@ public class ToDoService {
     public MessageResponse updateToDo(UpdateToDoRequest dto, Long id){
         ToDoList toDoList = toDoListRepository.findById(id)
                 .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
+        checkPermission(toDoList);
         toDoList.setContents(dto.getContents());
         return MessageResponse.builder()
                 .message(id + "번 TodoList를 수정했습니다.")
                 .build();
     }
     @Transactional
-    public MessageResponse success(SuccessBoolean dto, Long id){
+    public MessageResponse deleteToDo(Long id){
         ToDoList toDoList = toDoListRepository.findById(id)
                 .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
-        toDoList.setIs_success(dto.getIs_success());
-        return MessageResponse.builder()
-                .message(id + "번 아이디의 투두의 완료여부가 " + dto.getIs_success() + "로 변경되었습니다.")
-                .build();
-    }
-    @Transactional
-    public MessageResponse deleteToDo(Long id){
+        checkPermission(toDoList);
         try {
-            toDoListRepository.deleteById(id);
+            toDoList.setIs_success(false);
         } catch (Exception e) {
             throw new BaseException(ErrorCode.NOT_FOUND);
         }
@@ -55,18 +55,27 @@ public class ToDoService {
                 .message(id + "번 TodoList가 삭제되었습니다.")
                 .build();
     }
-    @Transactional
-    public MessageResponse deleteAll(){
-        toDoListRepository.deleteAll();
-        return MessageResponse.builder()
-                .message("모든 TodoList가 삭제 되었습니다.")
-                .build();
+    public List<ToDoListResponse> getToDo(){
+        List<ToDoListResponse> toDoListResponses = new ArrayList<>();
+        List<ToDoList> toDoLists = toDoListRepository.findByMember(authenticationFacade.getCurrentMember());
+        for (ToDoList todo : toDoLists) {
+            if(todo.getIs_success()) {
+                toDoListResponses.add(new ToDoListResponse(todo.getId(), todo.getContents()));
+            }
+        }
+//        return toDoLists.stream().map(toDoList -> new ToDoListResponse(toDoList.getContents())).collect(Collectors.toList());
+        return toDoListResponses;
     }
-    public List<ToDoList> getToDo(){
-        return toDoListRepository.findAll();
-    }
-    public ToDoList getToDo(Long id){
-        return toDoListRepository.findById(id)
+    public ToDoListResponse getToDo(Long id){
+        ToDoList toDoList = toDoListRepository.findById(id)
                 .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
+        checkPermission(toDoList);
+        return new ToDoListResponse(toDoList.getId(), toDoList.getContents());
+    }
+    private void checkPermission(ToDoList toDoList){
+        Member currentMember = authenticationFacade.getCurrentMember();
+        if(!toDoList.getMember().equals(currentMember)){
+            throw new BaseException(ErrorCode.FORBIDDEN);
+        }
     }
 }
