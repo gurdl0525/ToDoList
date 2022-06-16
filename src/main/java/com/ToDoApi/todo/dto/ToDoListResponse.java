d745c37746686247d74fe19bcda8fea206a7f68a
squall7011 <gurdl7011@dsm.hs.kr>
2022-06-16 22:58:08 +0900

    |UPDATE| AllArgsConstructor 어노테이션 추가

--- src/main/java/com/ToDoApi/todo/dto/ToDoListResponse.java
+++ src/main/java/com/ToDoApi/todo/dto/ToDoListResponse.java
@@ -3,14 +3,13 @@ package com.ToDoApi.todo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 public class ToDoListResponse {
     private final Long id;
     private final String contents;
-//    public ToDoListResponse(String contents) {
+//    public ToDoListResponse(Long id, String contents) {
+//        this.id = id;
 //        this.contents = contents;
 //    }
 }
